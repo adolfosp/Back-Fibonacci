d3754a61227398f28d7d8b3b5f09866d638d0736
Adolfo Poiatti <adolfo.poiatti@inovafarma.com.br>
2021-12-28 09:46:42 -0300

    feat: adiciona metodo de calcular sequencia fibonacci

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(CalculoFibonacciRecursivo(i));
+        }
+
+        public static int CalculoFibonacciRecursivo(int numero)
+        {
+            if (numero < 2)
+                return numero;
+
+            return CalculoFibonacciRecursivo(numero - 1) + CalculoFibonacciRecursivo(numero - 2);
+        }
+
+    }
+}
+
+  
+
